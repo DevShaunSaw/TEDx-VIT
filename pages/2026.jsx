a70c029078a038aa9b1938294d21e0d5000f71da
DevShaunSaw <workwithshaun@icloud.com>
2026-03-11 22:41:25 +0530

    Changes Made for Webhook

--- pages/2026.jsx
+++ pages/2026.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/widgets/navbar/Navbar'
 import Cards from '../components/speaker/Card'
 import styles from '../styles/2024.module.css'
 import Image from 'next/image'
+import { tedx2025 } from '../components/constants/constants'
 import MiniFooter from '../components/widgets/minifooter/MiniFooter'
 import Timeline from '../components/timeline/Timeline'
 
@@ -28,7 +29,7 @@ export default function Edition6 () {
                   Brochure
                 </button>
                 </a> */}
-                <Link href='/get-ticket'>
+                <Link href='/seat-selection'>
                 <button className={styles.yearlocation}>
                     Get Tickets
                 </button>
@@ -43,9 +44,7 @@ export default function Edition6 () {
                 />
               </div>
             </div>
-            <iframe src="https://konfhub.com/widget/tedx-vit-2026-touching-the-soul?desc=false&secondaryBg=ff0000&ticketBg=ff0000&borderCl=ff0000&bg=000000&fontColor=ffffff&ticketCl=ffffff&btnColor=000000&fontFamily=Figtree&borderRadius=24&widget_type=quick&screen=2&tickets=83152&ticketId=83152%7C1" id="konfhub-widget" title="Register for TEDx VIT 2026 - Touching The Soul" width="100%" height="750"></iframe>
-            <iframe src="https://konfhub.com/widget/tedx-vit-2026-touching-the-soul?desc=false&secondaryBg=F7F7F7&ticketBg=F7F7F7&borderCl=F7F7F7&bg=FFFFFF&fontColor=1e1f24&ticketCl=1e1f24&btnColor=002E6E&fontFamily=Figtree&borderRadius=24&widget_type=standard&tickets=84208&ticketId=84208%7C1" id="konfhub-widget" title="Register for TEDx VIT 2026 - Touching The Soul" width="100%" height="520"></iframe>
-            {/* <div className={styles.speaktitle}>
+            <div className={styles.speaktitle}>
                 <h2>Speakers</h2>
             </div>
             <div className={styles.speakers}>
@@ -63,7 +62,7 @@ export default function Edition6 () {
                     </div>
                     ))}
                 </div>
-            </div> */}
+            </div>
           {/* <Timeline/> */}
         </div>
       <MiniFooter/>
