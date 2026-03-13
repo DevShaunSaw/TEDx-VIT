f9445ffdd534493779d196cb42e855baba8e60e0
DevShaunSaw <workwithshaun@icloud.com>
2026-03-13 10:26:56 +0530

    Penultimate Commit

--- pages/handle-booking.js
+++ pages/handle-booking.js
@@ -29,8 +29,39 @@ export default function HandleBooking() {
     }
   }, [status, router]);
   return (
-    <div style={{ textAlign: "center", marginTop: "40vh", color: "white" }}>
-      Checking booking...
-    </div>
+    <>
+      <Navbar />
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#0B0B0B",
+        }}
+      >
+        <div
+          style={{
+            width: "50px",
+            height: "50px",
+            border: "5px solid rgba(255,255,255,0.2)",
+            borderTop: "5px solid #E62B1E",
+            borderRadius: "50%",
+            animation: "spin 1s linear infinite",
+          }}
+        />
+
+        <style jsx>{`
+          @keyframes spin {
+            0% {
+              transform: rotate(0deg);
+            }
+            100% {
+              transform: rotate(360deg);
+            }
+          }
+        `}</style>
+      </div>
+    </>
   );
 }
